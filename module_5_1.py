3731cf8630075b95d1689187a39877d84ef882fd
Anatolii-Sheiko <danik.shagun@gmail.com>
2024-12-09 17:47:52 +0300

    new uppdate 1

--- module_5_1.py
+++ module_5_1.py
@@ -4,12 +4,12 @@ class House:
         self.number_of_floors =  number_of_floors
 
     def go_to(self, new_floor):
-        for i in range(1, new_floor + 1):
+
             if 1 <= new_floor <= self.number_of_floors:
-                print(i)
+                for i in range(1, new_floor + 1):print(i)
             else:
                 print('Такого этажа не существует')
-                break
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
