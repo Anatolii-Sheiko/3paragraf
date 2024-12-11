973466f3f669125dfac6d3b4354764dc9a362fda
Anatolii-Sheiko <danik.shagun@gmail.com>
2024-12-11 18:33:57 +0300

    new uppdate 1

--- module_5_3.py
+++ module_5_3.py
@@ -13,7 +13,10 @@ class House:
         return F"Название: {self.name}, количество этажей: {self.number_of_floors}"
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
